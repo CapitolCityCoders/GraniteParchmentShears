a12760b4cb3bcbdc8c5a081f79b5dab875c7bfed
Amanda Fillip <afillip@windstream.net>
2016-07-08 13:06:07 -0500

    rebased

--- client/main.js
+++ client/main.js
@@ -5,15 +5,11 @@ import { Router, Route, browserHistory } from 'react-router'
 
 // Import components
 import Menu from './components/Menu';
-<<<<<<< 9ad761f52228bd36a1d98834800c9f419dfeebab
 import BattleContainer from './components/BattleContainer';
-=======
->>>>>>> Battle Container is now a class with view and constructor
 
 // Render that component to the DOM!
 ReactDOM.render((
   <Router history={browserHistory}>
-<<<<<<< 9ad761f52228bd36a1d98834800c9f419dfeebab
     <Route path="/" component={Menu} />
     <Route path="/battle" component={BattleContainer} />
 

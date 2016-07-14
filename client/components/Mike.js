f068b9eb6758204636ed4a00df78caaa8cc0f93e
Michael Fleming <flemingatx@gmail.com>
2016-07-14 14:59:10 -0500

    First commit, opponent appears in battle container

--- client/components/Mike.js
+++ client/components/Mike.js
@@ -3,11 +3,17 @@ import React from 'react'
 export default class Mike extends React.Component {
   render() {
     return (
-      <div className="playerTwo six columns">
+      <div className="playerOne six columns">
         <div>
-          <h5>Player Two Nickname</h5>
+          <h5>Player One Nickname</h5>
         </div>
         <div className="arena container">
+          <img src = {this.props.icon}/>
+        </div>
+        <div>
+          <button>Rock</button>
+          <button>Paper</button>
+          <button>Scissors</button>
         </div>
       </div>
     );

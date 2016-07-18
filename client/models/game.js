d3ca5904b89a8d0e356a657042aa3939c04ba57d
Shane McQuerter <shanemcquerter@gmail.com>
2016-07-18 11:41:24 -0500

    Delete old comments about url params

--- client/models/game.js
+++ client/models/game.js
@@ -1,11 +1,5 @@
 import fetch from 'isomorphic-fetch';
 
-// Is there a way to make GET requests AND send on db query search
-// parameters without having to make it a POST request and include
-// those parameters as properties in the body?
-  // Yes, you put the info on the url to send it through and then
-  // use "req.params" or "req.query" to grab the information with express
-
 //------update player's status to move-------//
 export function playerMove(move, userId){
 	return fetch('/api/userMove',{

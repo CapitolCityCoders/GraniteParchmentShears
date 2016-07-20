03ce1d0fb14baa39276aa9042dd5c644c09a5255
Tyler McCarthy <TMcCarthy8@gmail.com>
2016-07-19 19:53:48 -0500

    almost working

---
+++ client/components/BattleChat.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default class BattleChat extends React.Component {
+
+	constructor(){
+    super();
+  }
+
+	render () {
+		// return (
+		//     <div>{this.props.messages.map((msg) => <div>{msg}</div>)}</div>
+  //   )
+
+    return(<div></div>)
+	}
+}
--- client/components/Chat.js
+++ client/components/Chat.js
@@ -1,11 +1,7 @@
 import React from 'react';
+import BattleChat from './BattleChat'
 
-// import * as Menu from '../models/menu';
-// import Lobby from './Lobby';
-// import BattleContainer from './BattleContainer';
-// import End from './End';
-
-export default class BattleContainer extends React.Component {
+export default class Chat extends React.Component {
 
 	constructor(){
     super();
@@ -16,27 +12,23 @@ export default class BattleContainer extends React.Component {
   }
 
   componentDidMount() {
-  	
+  	var self = this
 		socket.on('chat message', function(msg){
-			console.log(msg)
-  		this.setState({messages: messages.push(msg)})
+			console.log("socket.on: ", msg)
+  		self.setState({messages: self.state.messages.concat(msg) })
 		});
-		
   }
 
 	render () {
 		return (
 		    <div className="chatBox">
-		    <div className="messages">{
-		    	this.state.messages.map((msg) =>  {
-		    		<div>{msg}</div>
-		    	})
-		    }</div>
+		      <BattleChat />
           <form>
           	<input value={this.state.chatText}
 							onChange={event => this.setState({chatText: event.target.value})}/>
 								<button type="button" onClick={() => {
-            			socket.emit('chat message', this.state.chatText)}}>send</button>
+            			console.log("socket.emit: ", this.state.chatText)
+            			socket.emit('chat message', this.state.chatText)  }}>send</button>
           </form>
         </div>
     )

5b05364aaf4d623e1262c38bfc90f7c756c2bc96
Christopher Wiles <chriswiles87@gmail.com>
2016-07-20 15:02:46 -0500

    formating

--- client/components/Chatbox.jsx
+++ client/components/Chatbox.jsx
@@ -1,85 +1,101 @@
-import React from 'react'
+import React from 'react';
 
 export default class Chatbox extends React.Component {
 
-  constructor(){
-    super();
-    this.state = {
-      username: '',
-      text: '',
-      messages: []
+    constructor() {
+        super();
+        this.state = {
+            messages: [],
+            text    : '',
+            username: ''
+        };
     }
-  }
 
-  componentDidMount () {
-    var self = this
-    this.fbUser = sessionStorage.getItem('fbUser');
-    this.setState({username: this.fbUser})
-		socket.on('Chatbox message', function(msg){
-			console.log("socket.on: ", msg)
-  		self.setState({messages: self.state.messages.concat(msg) })
-		});
-  }
+    componentDidMount() {
+        var self = this;
+        this.fbUser = sessionStorage.getItem('fbUser');
+        this.setState({username: this.fbUser});
+        socket.on('Chatbox message',
+            function (msg) {
+            console.log("socket.on: ", msg);
+            self.setState({
+                messages: self
+                    .state
+                    .messages
+                    .concat(msg)
+            });
+        }
+        );
+    }
 
-  _handleSubmit(event) {
+    _handleSubmit(event) {
 
-    event.preventDefault();
-    socket.emit('Chatbox message', {name: this.state.username  || 'Anon', message: this.state.text})
-    // send request to the socket.io
+        event.preventDefault();
+        socket.emit('Chatbox message', {
+            message: this.state.text,
+            name   : this.state.username || 'Anon'
+        });
+        // send request to the socket.io
 
-   }
+    }
 
-
-  render() {
-    return (
-      <div>
-        <Messages messages={this.state.messages}/>
-        <form onSubmit={this._handleSubmit.bind(this)}>
-           <input
-             type="text"
-             value={this.state.text}
-             className="u-full-width"
-             placeholder="chat..."
-             id="chatInput"
-             onChange={event => this.setState({text: event.target.value})}
-           />
-           <input type="submit" style={{visibility: 'hidden'}} ></input>
-        </form>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div>
+                <Messages messages={this.state.messages}/>
+                <form
+                    onsubmit={this._handlesubmit.bind(this)}>
+                    <input
+                        type="text"
+                        value={this.state.text}
+                        className="u-full-width"
+                        placeholder="chat..."
+                        id="chatInput"
+                        onchange={event => this.setstate({text: event.target.value})}/>
+                    <input
+                        type="submit"
+                        style={{
+                        visibility: 'hidden'
+                    }}></input>
+                </form>
+            </div>
+        );
+    }
 }
 
 class Messages extends React.Component {
 
-  _createMessages() {
-    return this.props.messages.map((msg, index) => {
-      return <Message key={index} name={msg.name} message={msg.message}/>
-    });
-  }
+    _createMessages() {
+        return this
+            .props
+            .messages
+            .map((msg, index) => {
+                return <Message key={index} name={msg.name} message={msg.message}/>;
+            });
+    }
 
-  render() {
-    return (
-      <div className="messages">
-          <table className="u-full-width">
-            <tbody>
-              {this._createMessages()}
-            </tbody>
-          </table>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className="messages">
+                <table className="u-full-width">
+                    <tbody>
+                        {this._createMessages()}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
 }
 
 class Message extends React.Component {
-  render() {
-    return (
+    render() {
+        return (
 
-          <tr className="message">
-            <td>{this.props.name}</td>
-            <td>{this.props.message}</td>
-          </tr>
+            <tr className="message">
+                <td>{this.props.name}</td>
+                <td>{this.props.message}</td>
+            </tr>
 
-    )
-  }
+        );
+    }
 }

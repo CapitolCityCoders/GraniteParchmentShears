ae9616ed411eff39b9a8659a44bc1f46791d900b
Burton Cooke <burtoncooke@yahoo.com>
2016-07-21 13:28:20 -0500

    intermediary commit

--- client/components/End.js
+++ client/components/End.js
@@ -1,7 +1,18 @@
 import React from 'react'
 import { Link } from 'react-router'
+import Menu from '../models/menu.js'
 //--------------------Game Over Page-------------//
 export default class End extends React.Component{
+
+  componentDidMount() {
+
+    console.log("showing game id in end.js:", this.props.gameId);
+    Menu.updateGameStatus(this.props.gameId, 'completed')
+      .then(function(data) {
+        console.log("updated status to complete!:", data);
+      })
+  }
+
   render() {
     return (
       <section className="narrative container six columns offset-by-three">

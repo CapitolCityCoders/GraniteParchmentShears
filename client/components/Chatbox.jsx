40209811f79d23d30011f705bd3a531fb09c7e1a
Christopher Wiles <chriswiles87@gmail.com>
2016-07-21 17:41:13 -0500

    chat fb picture

--- client/components/Chatbox.jsx
+++ client/components/Chatbox.jsx
@@ -24,6 +24,7 @@ export default class Chatbox extends React.Component {
     socket.emit("Chatbox message", {
       name   : sessionStorage.getItem("fbUser") || "Anon",
       message: this.state.text,
+      imgUrl    : sessionStorage.getItem("imgUrl") || "http://placehold.it/50/55C1E7/fff&text=Anon"
     })
     // send request to the socket.io
     this.setState({
@@ -34,21 +35,23 @@ export default class Chatbox extends React.Component {
   render() {
 
     return (
-      <div >
-        <form onSubmit={this._handleSubmit.bind(this)}>
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              onFocus={event => this.value = ""}
-              value={this.state.text}
-              placeholder="chat..."
-              id="chatInput"
-              onChange={event => this.setState({text: event.target.value})}
-            />
-          </div>
-        </form>
-        <Messages messages={this.state.messages}/>
+      <div className="row">
+        <div className="col-md-5">
+          <Messages messages={this.state.messages}/>
+          <form onSubmit={this._handleSubmit.bind(this)}>
+              <div className="input-group">
+              <input
+                type="text"
+                className="form-control input-sm"
+                onFocus={event => this.value = ""}
+                value={this.state.text}
+                placeholder="Type your message here..."
+                id="btn-input"
+                onChange={event => this.setState({text: event.target.value})}
+              />
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
@@ -57,29 +60,24 @@ export default class Chatbox extends React.Component {
 class Messages extends React.Component {
 
   _createMessages() {
-    var msgTrim;
-    if (this.props.messages.length > 10) {
-      msgTrim = this.props.messages.slice(this.props.messages.length - 10);
-    } else {
-      msgTrim = this.props.messages;
-    }
-    return msgTrim.map((msg,
-    index) => {
+    return this.props.messages.map((msg, index) => {
       return <Message
         key={index}
         name={msg.name.substring(0, 15)}
-        message={msg.message.substring(0, 31)}/>
+        message={msg.message.substring(0, 31)}
+        imgUrl={msg.imgUrl}
+        />
     });
   }
 
   render() {
     return (
-      <div className="messages">
-        <table className="table table-hover">
-          <tbody>
+      <div className="panel panel-primary">
+        <div className="panel-body">
+          <ul className="chat">
             {this._createMessages()}
-          </tbody>
-        </table>
+          </ul>
+        </div>
       </div>
     )
   }
@@ -88,10 +86,23 @@ class Messages extends React.Component {
 class Message extends React.Component {
   render() {
     return (
-      <tr className="message">
-        <td className="text-left">{this.props.name}</td>
-        <td className="text-right">{this.props.message}</td>
-      </tr>
+      <li className="left clearfix">
+        <span className="chat-img pull-left">
+          <img
+            height="71"
+            width="71"
+            src={this.props.imgUrl}
+            alt="User Avatar"
+            className="img-circle"/>
+        </span>
+        <div className="chat-body clearfix">
+          <div className="header">
+            <strong className="primary-font">{this.props.name}</strong>
+            <small className="pull-right text-muted">12 mins ago</small>
+          </div>
+          <p className="pull-right">{this.props.message}</p>
+        </div>
+      </li>
     )
   }
 }
